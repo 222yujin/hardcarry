9c5050ed4346378e2c09e19d1fbef84ca00afa90
222yujin <222yujin@naver.com>
2021-11-10 14:08:31 +0900

    cors 설정 변경

--- main/app.py
+++ main/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route('/')
 def home():
