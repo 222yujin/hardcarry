7547c68bb937ac7a1c2517ac423aa90137ed4344
222yujin <222yujin@naver.com>
2021-11-11 15:15:04 +0900

    cors 변경

--- main/app.py
+++ main/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"*": {"origins": "*"}})
+CORS(app)
 
 
 client = MongoClient('mongodb://test:test@localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
