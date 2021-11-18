c5b15cdb2fabeb8e11dced100625c2110c77548f
222yujin <222yujin@naver.com>
2021-11-18 14:59:36 +0900

    get 추가

--- myproject.py
+++ myproject.py
@@ -17,6 +17,31 @@ def home():
 
 
 # test 점수 계산해서 결과 주기
+@app.route('/result', methods=['GET'])
+def test_result_get():
+    result_score = request.args.get('result_array')
+    print(result_score)
+    result_score = result_score.split('_')
+    print(result_score)
+    result_type = 0
+
+    if result_score[3] == '1' or (result_score[4] == '1' and result_score[5] == '1'):
+        result_type = 2
+        if result_score[6] == '1' or (result_score[7] == '1' and result_score[8] == '1'):
+            result_type = 3
+            if result_score[9] == '1' or (result_score[10] == '1' and result_score[11] == '1'):
+                result_type = 4
+    else:
+        result_type = 1
+
+    category = db.category.find_one({"type": result_type}, {"_id": False})
+    category["programs"] = []
+    for i in category["id"]:
+        category["programs"].append(db.program.find_one({"id": i}, {"_id": False}))
+    print(category)
+    return jsonify({"isSuccess": "success", "data": category})
+
+# test 점수 계산해서 결과 주기
 @app.route('/result', methods=['POST'])
 def test_result():
     result_score = request.form.get('result_array')
